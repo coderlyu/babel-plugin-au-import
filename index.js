fc5edc7c310ae10b63f7ccfacbcfb52c338f6963
刘誉 <974257574@qq.com>
2021-08-05 08:47:16 +0800

    refactor: 添加注释

--- index.js
+++ index.js
@@ -29,12 +29,14 @@ module.exports = function ({ type }) {
         if (!node) return
         const { source: { value: libName }, specifiers = [] } = node
         const imports = []
-        if (defaultOpts.libraryName === libName) {
+        if (defaultOpts.libraryName === libName) { // import 引入的库名和 libraryName 相同进入
           specifiers.forEach(item => {
             if (t.isImportSpecifier(item)) {
               const { imported: { name: importedName }, local: { name: localName } } = item
               if (!importedName || !localName) return
-              if (path.scope.getBinding(localName).references === 0) return
+              if (path.scope.getBinding(localName).references === 0) return // 当前变量没有使用到，直接抛弃
+              // 组件
+              // import { Roll } from 'au-ui' => import Roll from 'au-ui/packages/roll
               imports.push(t.importDeclaration([t.importDefaultSpecifier(t.identifier(localName))], t.StringLiteral(defaultOpts.customName(importedName))))
               // css
               if (typeof defaultOpts.style === 'function' && defaultOpts.style(importedName)) {
@@ -42,7 +44,7 @@ module.exports = function ({ type }) {
               }
             }
           })
-          path.replaceWithMultiple(imports)
+          path.replaceWithMultiple(imports) // 替换成改变后的引入方式
         }
       }
     }
